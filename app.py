7a43d09c2cf3cbcce7a4be7adec221173b32855c
NavTheRaj <navrajkhanal61@gmail.com>
2020-08-04 15:11:38 +0545

    Committing aall files after testing

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_socketio import SocketIO,send,emit
+
+list_task = []
+data_id = 0
+# create flask app
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret'
+
+#Creating the socket object
+socketio = SocketIO(app,cors_allowed_origins="*")
+
+@socketio.on('message')
+def handleMessage(task):
+    list_task.append(task)
+    print(list_task)
+    data_id = len(list_task)
+    print(f'Task no:{data_id} {task}')
+    #broadcasting the meesage to all connected users
+    send(task,broadcast=True)
+
+if __name__ == '__main__':
+    socketio.run(app)
